6fe32cb6e06a48aac8ec134203187751ce00bbd4
torvalds <torvaldz@gmail.com>
2018-05-08 16:04:29 +0200

    Unificate versione ldap e non se esiste nella config il component ldap uso ldap, altrimenti no

--- models/forms/LoginForm.php
+++ models/forms/LoginForm.php
@@ -55,7 +55,7 @@ class LoginForm extends Model
 		{
 
 			$user = $this->getUser();
-			if($this->username=='superadmin' || array_key_exists('ldap',yii::$app->components)){
+			if($this->username=='superadmin' || array_key_exists('options',yii::$app->components->lap)){
                 $valido=!$user || !$user->validatePassword($this->password);
             }else{
                 $valido=!$user || !\Yii::$app->ldap->authenticate($this->username,$this->password);
