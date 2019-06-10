6f868f84c5ce5d0ecc2b13b4ee95e6e3a41ba435
Marco Migozzi <torvaldz@gmail.com>
2019-06-10 11:01:46 +0200

    Aggiunto login.js

--- assets/LoginAsset.php
+++ assets/LoginAsset.php
@@ -7,7 +7,7 @@ use yii\web\AssetBundle;
 
 class LoginAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/module-user-management/assets/';
+    public $sourcePath = '@vendor/webvimark/module-user-management/assets/';
 
     public $js = [
 		"login.js"
